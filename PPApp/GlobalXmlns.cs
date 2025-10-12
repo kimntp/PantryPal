82e99d5b90ff7456c82ede6518649f6d4f27b8e6
Kim Nguyen <kimolps1@icloud.com>
2025-10-11 21:41:32 -0500

    Delete PPApp directory
    will repush with gitignore

--- PPApp/GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "PPApp")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "PPApp.Pages")]
--- PPApp/Recipe.cs
+++
@@ -1,9 +0,0 @@
-namespace PPApp.model //name spaces are like a folder for your code
-{
-    public class Recipe
-    {
-        public string Name { get; set; }
-        public string Url { get; set; } // Optional, if you have recipe links
-        public List<string> Ingredients { get; set; } // Optional
-    }
-}
