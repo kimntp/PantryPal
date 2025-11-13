014651ede83a4f9db258b1fd9588b16f92331b7a
Glen <g.henson1628@gmail.com>
2025-11-13 17:02:21 -0600

    Completes the ingredient getter. Now fully functional.

--- ingredientGetter/ingredientGetter.py
+++ ingredientGetter/ingredientGetter.py
@@ -1,11 +1,18 @@
 import json
 
+# IMPORTANT: replace the file paths below with the appropriate locations on your own computer
+# if you want to run the program on your own computer 
+
+#input file
+RECIPES_FILE_PATH = "/Users/gh3work/code_projects/PantryPal/ingredientGetter/all_recipes.json"
+#output_file
+INGREDIENTS_FILE_PATH = "/Users/gh3work/code_projects/PantryPal/ingredientGetter/ingredients.json"
+
 ingredients_list = []
-is_meat = []
-has_gluten = []
+staged_ingredients = []
 
 # fetches the individual ingredients from each recipe ignoring duplicates
-with open("/Users/gh3work/code_projects/PantryPal/ingredientGetter/allRecipes.json", "r") as dataset:
+with open(RECIPES_FILE_PATH, "r") as dataset:
     recipe_list = json.load(dataset)
     for recipe in recipe_list:
         ingredients = recipe['ingredients']
@@ -16,28 +23,32 @@ with open("/Users/gh3work/code_projects/PantryPal/ingredientGetter/allRecipes.js
                 continue
 
 i = 0
-while(i < 30):
+is_meat = False
+has_gluten = False
+
+for ingredient in ingredients_list:
     meat = input("Is " + ingredients_list[i] + " a meat? [y/n]  ")
     gluten = input("Does " + ingredients_list[i] + " contain gluten? [y/n]  ")
-
-    if(meat == 'y'):
-        is_meat.append(ingredients_list[i])
-    if(gluten == "y"):
-        has_gluten.append(ingredients_list[i])
-
-    i+=1
     
-# Adds each unique indvidual ingredient to a new database called "ingredients.txt"
-with open("/Users/gh3work/code_projects/PantryPal/ingredientGetter/all_ingredients.txt", "w") as all_ingredients:
-    for ingredient in ingredients_list:
-        print(ingredient, file=all_ingredients)
+    if(meat == 'y'):
+        is_meat = True
+    if(gluten == 'y'):
+        has_gluten = True
+    
+    formatted_ingredient = {
+        "ingredient": ingredients_list[i],
+        "is_meat": is_meat,
+        "has_gluten": has_gluten
+    }
 
-# Adds each meat in "meats" to a new database called "meats.txt"
-with open("/Users/gh3work/code_projects/PantryPal/ingredientGetter/meats.txt", "w") as meats:
-    for meat in is_meat:
-        print(meat, file=meats)
+    staged_ingredients.append(formatted_ingredient)
+    if(is_meat == True):
+        is_meat = False
+    if(has_gluten == True):
+        has_gluten = False
+    
+    i+=1                                                                                       
 
-# Adds each gluten in "glutens" to a new database called "glutens.txt"
-with open("/Users/gh3work/code_projects/PantryPal/ingredientGetter/glutens.txt", "w") as glutens:
-    for gluten in has_gluten:
-        print(gluten, file=glutens)
+# Adds each unique indvidual ingredient to a new database called "ingredients.json"
+with open(INGREDIENTS_FILE_PATH, "w") as ingredients_file:
+    json.dump(staged_ingredients, ingredients_file, indent=4)
