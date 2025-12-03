e77745006e6096cefb17355aa0fe044ccd778b3d
Glen <g.henson1628@gmail.com>
2025-12-02 19:17:15 -0600

    adds fancy app icon

--- PPApp/Model/Recipe.cs
+++ PPApp/Model/Recipe.cs
@@ -6,6 +6,5 @@ namespace PPApp.Model //name spaces are like a folder for your code
         public string Url { get; set; }
         public List<string> Ingredients { get; set; }
         public string RecipeID { get; set; }
-        public string ImageUrl { get; set; }
     }
 }
